74947cd031c1b76cf0c93d5c920f27f8961707cb
francisldn <fnhh04@gmail.com>
2022-01-16 15:12:08 +0000

    update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const disputeNFTMarket = '0xdcab8c7b03964d25952b539d95deef2b14c49f30'
-  export const disputeNFT = '0xa0fc7a9e3d1efc77ac46f35ef1998da02c9deeb9'
-  export const escrow = '0x62c2f40a03b510c155bf8ad100bb19edc6710c86'
+  export const disputeNFTMarket = '0xcdb939a9f58874fcad125995d62d8a5b54527bbf'
+  export const disputeNFT = '0x02751cbbc2e011856dd0e89bd05afb2f9e52268c'
+  export const escrow = '0x02751cbbc2e011856dd0e89bd05afb2f9e52268c'
