251debb03a24cc5419dec2fd011fb33345e55a5e
francisldn <fnhh04@gmail.com>
2022-01-16 11:27:05 +0000

    config files edit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-etherscan');
 require('dotenv').config()
 
 module.exports = {
@@ -7,11 +8,11 @@ module.exports = {
     localhost: {
     },
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_RPC_KEY}`,
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_RPC_KEY}`,
       accounts:[process.env.PRIVATE_KEY]
     },
     mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_RPC_KEY}`,
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.ALCHEMY_RPC_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
     },
     rinkeby: {
@@ -25,5 +26,8 @@ module.exports = {
       enabled: true,
       runs: 200
     }
-  }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
 };
